675dc732b7dc55b30bc09eadf47ec4d475cd30c9
abdurrahman6489 <120268868+abdurrahman6489@users.noreply.github.com>
2023-06-05 00:42:15 +0530

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -3,7 +3,6 @@ import ListRender from "./ListRender";
 
 const Today = ({ list }) => {
   const date = new Date();
-  console.log(list);
   return (
     <div id="today-list">
       <ListRender list={list} />
