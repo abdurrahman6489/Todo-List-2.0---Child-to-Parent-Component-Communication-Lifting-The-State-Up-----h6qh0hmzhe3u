4b755acd0525943c69ab47ef595157b873223bd4
abdurrahman6489 <120268868+abdurrahman6489@users.noreply.github.com>
2023-06-09 12:25:00 +0530

    Update MainSection.js

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -9,29 +9,20 @@ const list = [
   { number: 3, title: "Should complete react", date: new Date("10/5/2022") },
 ];
 
-const MainSection = ({ active }) => {
-  const [originalList, setOriginalList] = useState(list);
-  function addTodo({ title, date }) {
-    setOriginalList((oldList) => [
-      ...oldList,
-      { number: oldList.length, title, date },
-    ]);
-  }
-  function countDays(date) {
-    let date1 = new Date();
-    let time = date - date1;
-    let days = parseInt(time / (24 * 60 * 60 * 1000));
-    return days;
-  }
-  const todayList = originalList.filter((item) => countDays(item.date) === 0);
-  const weekLIst = originalList.filter(
-    (item) => countDays(item.date) >= 0 && countDays(item.date) <= 7
-  );
+const MainSection = (props) => {
+  const [filteredList, setFilteredList] = useState(list);
+  const addToList = (obj) => {
+    list.push(obj);
+    setFilteredList(list);
+  };
+
   return (
     <div className="main-section">
-      {active === "INBOX" && <Inbox list={originalList} append={addTodo} />}
-      {active === "TODAY" && <Today list={todayList} />}
-      {active === "NEXT" && <Next7Days list={weekLIst} />}
+      {props.active === "INBOX" && (
+        <Inbox list={filteredList} append={addToList} />
+      )}
+      {props.active === "TODAY" && <Today list={filteredList} />}
+      {props.active === "NEXT" && <Next7Days list={filteredList} />}
     </div>
   );
 };
