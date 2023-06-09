f7f89ab2b8438eea712f2b25bc798dbffcca9c93
abdurrahman6489 <120268868+abdurrahman6489@users.noreply.github.com>
2023-06-09 12:21:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,26 +4,24 @@ import MainSection from './MainSection';
 import Header from './Header';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../styles/App.css';
+const App = () => {
 
-
-function App() {
-  const [activeTab, setActiveTab] = useState("INBOX");
-  function handleChange(tab) {
-    setActiveTab(tab);
-  }
+  const [active, setActive] = useState("INBOX");
 
   return (
     <div id="main">
-      <Header />
-      <div className="row">
-        <div className="col-md-2">
-          <SideNav change={handleChange} />
+    <Header />
+      <div className='row'>
+        <div className='col-md-2'>
+          <SideNav change={setActive}/>
         </div>
-        <div className="col-md-10">
-          <MainSection active={activeTab} />
+        <div className='col-md-10'>
+          <MainSection active = {active}/>
         </div>
       </div>
     </div>
-  );
+  )
 }
+
+
 export default App;
