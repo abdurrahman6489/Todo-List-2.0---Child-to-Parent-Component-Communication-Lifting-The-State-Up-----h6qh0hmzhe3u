d77078cf2e6535dacb37ac4c69593c59a603894a
abdurrahman6489 <120268868+abdurrahman6489@users.noreply.github.com>
2023-06-12 22:27:10 +0530

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,24 +1,11 @@
 import React from "react";
 import ListRender from "./ListRender";
 
-const Today = (props) => {
+const Today = ({ list }) => {
   const date = new Date();
-  const filteredList = props.list.filter((task) => {
-    if (date.getFullYear() !== task.date.getFullYear()) {
-      return false;
-    }
-    if (date.getMonth() !== task.date.getMonth()) {
-      return false;
-    }
-    if (date.getDate() !== task.date.getDate()) {
-      return false;
-    }
-    return true;
-  });
-
   return (
     <div id="today-list">
-      <ListRender list={filteredList} />
+      <ListRender list={list} />
     </div>
   );
 };
